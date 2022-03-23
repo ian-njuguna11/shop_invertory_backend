25a0c7ab65dbcade89742b1b85544a3cf145e9ee
Ian-Njuguna <njugunaayan@gmail.com>
2022-03-23 21:32:16 +0300

    Initial Commit

---
+++ src/test/java/com/shop_inventory/ayan/ShopInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_inventory.ayan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
